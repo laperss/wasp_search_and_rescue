3b6f293c44525bec74522bb7837bc095bb7f30f9
Linnea Persson <laperss@kth.se>
2016-12-11 21:31:54 +0100

    update name and information

--- planning/run_planner.sh
+++ planning/run_planner.sh
@@ -2,14 +2,14 @@ trap "kill 0" SIGINT
 
 sudo service mongodb stop
 echo "Launch interfaced planning system"
-roslaunch kth2_planning interfaced_planning_system_linnea.launch &
+roslaunch drone_planning start_planning_system.launch &
 sleep 2s
 echo "Add knowledge"
 (cd /home/laperss/catkin_ws/src/ascourse/planning/scripts/ && python add_knowledge_linnea.py &)
 sleep 2s
 echo "Launch rqt"
 rqt --standalone rosplan_rqt.dispatcher.ROSPlanDispatcher &
-sleep 2s
+sleep 1s
 echo "Run planner"
 rosservice call /kcl_rosplan/planning_server
 
