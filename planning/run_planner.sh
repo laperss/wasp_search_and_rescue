846abb4182f3f5a8d167bcef3256d8860cfe6c46
Linnea Persson <laperss@kth.se>
2018-04-03 17:37:53 +0200

    Update run_planner.sh

--- planning/run_planner.sh
+++ planning/run_planner.sh
@@ -1,11 +1,12 @@
 trap "kill 0" SIGINT
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 sudo service mongodb stop
 echo "Launch interfaced planning system"
 roslaunch drone_planning start_planning_system.launch &
 sleep 2s
 echo "Add knowledge"
-(cd /home/laperss/catkin_ws/src/ascourse/planning/scripts/ && python add_knowledge_linnea.py &)
+(cd "$parent_path/scripts/" && python add_knowledge_linnea.py &)
 sleep 2s
 echo "Launch rqt"
 rqt --standalone rosplan_rqt.dispatcher.ROSPlanDispatcher &
